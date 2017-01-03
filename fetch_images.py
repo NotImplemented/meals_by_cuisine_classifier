2b166e321d8d4111ecea5779b2b6ff2f54cd8232
Mikhail_Kaspiarovich <kasperovichm@gmail.com>
2017-01-03 22:20:16 +0300

    Full dataset download

--- fetch_images.py
+++ fetch_images.py
@@ -1,4 +1,9 @@
-import httplib, urllib, base64
+import httplib
+import requests
+import urllib
+import time
+import json
+import os
 
 headers = {
     # Request headers
@@ -6,18 +11,98 @@ headers = {
     'Ocp-Apim-Subscription-Key': '66c7302b20914adc9e700cd374f24744',
 }
 
-query = 'chinese+restaurants'
 
-try:
-    conn = httplib.HTTPSConnection('api.cognitive.microsoft.com')
-    conn.request("POST", "/bing/v5.0/images/search?q=" + query, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
+cuisines = [
+    'italian',
+    'indian',
+    'mexican',
+    'thai',
+    'chinese',
+    'lebanese',
+    'greek',
+    'japanese',
+    'french',
+    'spanish',
+    'russian',
+    'english',
+    'turkish',
+    'caribbean']
 
-    conn.close()
+sample_size = 512
+batch_size = 128
+retries = 6
+retry_timeout = 4
 
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+for cuisine in cuisines:
 
+    path = cuisine + '_restaurants'
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise
 
+    samples_left = sample_size
+    offset = 0
+    query = cuisine + '+restaurant+exterior'
+    index = 0
+
+    print 'Preparing data for cuisine: ' + cuisine
+
+    while samples_left > 0:
+
+        count = min(samples_left, batch_size)
+        connection = None
+
+        try:
+            connection = httplib.HTTPSConnection('api.cognitive.microsoft.com')
+            request = "/bing/v5.0/images/search?q={}&imageType=Photo&count={}&offset={}".format(query, count, offset)
+            print "Sending request: '{}'".format(request)
+            connection.request("POST", request, "{body}", headers)
+            response = connection.getresponse()
+            data = response.read()
+
+            images = json.loads(data)
+            images = images['value']
+
+            samples_left -= len(images)
+            offset += len(images)
+
+            for image in images:
+
+                url = image['contentUrl']
+                format = image['encodingFormat']
+                name = '{0}{1:04d}'.format(cuisine, index)
+
+                location = os.path.join(path, '{name}.{extension}'.format(name = name, extension = format))
+
+                if not os.path.isfile(location):
+
+                    print 'Downloading image #{}: {}'.format(index, url)
+                    print 'Image location: ' + location
+
+                    retry = 0
+                    response = None
+                    while retry < retries :
+
+                        try:
+                            response = requests.get(url)
+                            break
+
+                        except requests.exceptions.ConnectionError:
+                            print 'Retry #{} downloading image from: {}'.format(retry + 1, url)
+                            retry += 1
+                            time.sleep(retry_timeout)
+                        except requests.exceptions.TooManyRedirects:
+                            print 'Too many redirects for: {}'.format(url)
+                            break
+
+                    if response and response.status_code == 200:
+                        with open(location, 'wb') as file:
+                            file.write(response.content)
+
+                index += 1
+
+        finally:
+            if connection:
+                connection.close()
